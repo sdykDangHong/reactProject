9bf5b8f4b01f89826d2748c86022a43e95e80bb6
党泓 <danghong@315pr.com>
2018-08-16 22:59:22 +0800

    公共组件管理

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter , Route , Switch , Redirect } from "react-router-dom";
+import { BrowserRouter , Route , Switch } from "react-router-dom";
 import "./static/common.less";
 import Home from "./components/home/home";
 import Login from "./components/login/login";
@@ -9,24 +9,31 @@ import About from "./components/about/about";
 import Team from "./components/team/team";
 import Recruitment from "./components/recruitment/recruitment";
 import FreemanHot from "./components/freeman_hot/freeman_hot";
-class RouterSet extends Component{
+class Customer extends Component{
     render(){
         return (
-            <div>
-                <Header/>
-                <Route path="/" exact render={()=> (
-                    <Redirect to='/homepage'/>
-                )}/>
-                <Route path="/homepage" component={Home}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/about" component={About}/>
-                <Route path="/development" component={About}/>
-                <Route path="/contact_us" component={About}/>
-                <Route path="/team" component={Team}/>
-                <Route path="/recruitment" component={Recruitment}/>
-                <Route path="/freeman_hot" component={FreemanHot}/>
-                {this.props.location.pathname!=='/homepage'?(<Footer/>):''}
-            </div>
+            <Switch>
+                <Route path="/" exact>
+                    <div>
+                        <Header {...this.props}/>
+                        <Home {...this.props}/>
+                    </div>
+                </Route>
+                <Route path="/">
+                    <div>
+                        <Header {...this.props}/>
+                        <Route path="/homepage" component={Home}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/development" component={About}/>
+                        <Route path="/contact_us" component={About}/>
+                        <Route path="/team" component={Team}/>
+                        <Route path="/recruitment" component={Recruitment}/>
+                        <Route path="/freeman_hot" component={FreemanHot}/>
+                        <Footer {...this.props}/>
+                    </div>
+                </Route>
+            </Switch>
         )
 
     }
@@ -36,8 +43,9 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <Switch>
-                    <RouterSet />
-                    <Redirect to="/" />
+
+                    <Customer />
+                    <Route component={Home}/>
                 </Switch>
             </BrowserRouter>
         );
